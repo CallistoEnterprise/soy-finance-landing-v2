e518470444b0a01a62b349142cec47c9f6162413
Viktor Peredera <vitia2282@gmail.com>
2023-06-29 14:32:35 +0300

    feat: remove locales from config

--- i18n.js
+++ i18n.js
@@ -1,5 +1,6 @@
 module.exports = {
-  locales: ["en", "uk", "fr", "ru", "ko", "zh-TW", "zh-CH"],
+  // locales: ["en", "uk", "fr", "ru", "ko", "zh-TW", "zh-CH"],
+  locales: ["en"],
   defaultLocale: "en",
   extensionsRgx: /\.page\.(tsx|ts|js|jsx)$/,
   pages: {
