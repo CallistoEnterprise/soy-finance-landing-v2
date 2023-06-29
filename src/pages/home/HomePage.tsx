90e307e43770ad082c064160bb72114a51ab4e39
Viktor Peredera <vitia2282@gmail.com>
2023-06-30 00:13:21 +0300

    test: check speed without all blocks

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,13 +1,13 @@
 import styles from "./HomePage.module.scss"
 import Layout from "../../shared/layouts/Layout";
-import Banner from "./components/Banner";
-import Stats from "./components/Stats";
-import DeFi from "./components/DeFi";
-import Staking from "./components/Staking";
-import Farming from "./components/Farming";
-import Burning from "./components/Burning";
-import BridgeBlock from "./components/BridgeBlock";
-import EmpowerGrowth from "./components/EmpowerGrowth";
+// import Banner from "./components/Banner";
+// import Stats from "./components/Stats";
+// import DeFi from "./components/DeFi";
+// import Staking from "./components/Staking";
+// import Farming from "./components/Farming";
+// import Burning from "./components/Burning";
+// import BridgeBlock from "./components/BridgeBlock";
+// import EmpowerGrowth from "./components/EmpowerGrowth";
 import JoinSlothiverse from "./components/JoinSlothiverse";
 
 export default function Home() {
@@ -15,18 +15,18 @@ export default function Home() {
       <main>
         <div className="container">
           <div className={styles.mainContent}>
-            <Banner/>
-            <Stats/>
-            <DeFi/>
-            <BridgeBlock/>
+            {/*<Banner/>*/}
+            {/*<Stats/>*/}
+            {/*<DeFi/>*/}
+            {/*<BridgeBlock/>*/}
 
-            <div className={styles.stakingFarming}>
-              <Staking/>
-              <Farming/>
-            </div>
+            {/*<div className={styles.stakingFarming}>*/}
+            {/*  <Staking/>*/}
+            {/*  <Farming/>*/}
+            {/*</div>*/}
 
-            <Burning/>
-            <EmpowerGrowth/>
+            {/*<Burning/>*/}
+            {/*<EmpowerGrowth/>*/}
             <JoinSlothiverse/>
           </div>
         </div>
