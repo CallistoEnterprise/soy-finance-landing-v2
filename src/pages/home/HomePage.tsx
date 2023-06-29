dbea2da7f3e197598c616308be218394582848c0
Viktor Peredera <vitia2282@gmail.com>
2023-06-30 02:51:15 +0300

    feat: minor fixes

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,33 +2,34 @@ import styles from "./HomePage.module.scss"
 import Layout from "../../shared/layouts/Layout";
 import Banner from "./components/Banner";
 import Stats from "./components/Stats";
-// import DeFi from "./components/DeFi";
-// import Staking from "./components/Staking";
-// import Farming from "./components/Farming";
-// import Burning from "./components/Burning";
-// import BridgeBlock from "./components/BridgeBlock";
-// import EmpowerGrowth from "./components/EmpowerGrowth";
+import DeFi from "./components/DeFi";
+import Staking from "./components/Staking";
+import Farming from "./components/Farming";
+import Burning from "./components/Burning";
+import BridgeBlock from "./components/BridgeBlock";
+import EmpowerGrowth from "./components/EmpowerGrowth";
 import JoinSlothiverse from "./components/JoinSlothiverse";
 
 export default function Home() {
-  return  <main>
-        <div className="container">
-          <div className={styles.mainContent}>
-            <Banner/>
-            <Stats/>
-            {/*<DeFi/>*/}
-            {/*<BridgeBlock/>*/}
+  return <Layout>
+    <main>
+      <div className="container">
+        <div className={styles.mainContent}>
+          <Banner/>
+          <Stats/>
+          <DeFi/>
+          <BridgeBlock/>
 
-            {/*<div className={styles.stakingFarming}>*/}
-            {/*  <Staking/>*/}
-            {/*  <Farming/>*/}
-            {/*</div>*/}
-
-            {/*<Burning/>*/}
-            {/*<EmpowerGrowth/>*/}
-            <JoinSlothiverse/>
+          <div className={styles.stakingFarming}>
+            <Staking/>
+            <Farming/>
           </div>
+
+          <Burning/>
+          <EmpowerGrowth/>
+          <JoinSlothiverse/>
         </div>
-      </main>
-    // </Layout>
+      </div>
+    </main>
+  </Layout>
 }
