d33223ead4fb7ef33fd760f8bcc31e88f9677c77
Viktor Peredera <vitia2282@gmail.com>
2023-06-30 19:32:20 +0300

    feat: update og meta

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -15,16 +15,16 @@ export default function App({Component, pageProps}: AppProps) {
 
       <meta property="og:site_name" content="Soy.Finance"/>
       <meta property="og:title" content="Soy.Finance | Where DeFi Meets Safety"/>
-      <meta property="og:url" content="https://soy-finance-landing.vercel.app"/>
+      <meta property="og:url" content="https://soy.finance"/>
       <meta property="og:description"
             content="Experience the future of DeFi with SOY Finance. Our unique insurance fund provides a safe and affordableenvironment for trading, farming, and staking."/>
-      <meta property="og:image" content="https://soy-finance-landing.vercel.app/og.jpg"/>
+      <meta property="og:image" content="https://soy.finance/og.jpg"/>
 
       <meta name="twitter:site" content="Soy.Finance"/>
       <meta property="twitter:title" content="Soy.Finance | Where DeFi Meets Safety"/>
       <meta property="twitter:description"
             content="Experience the future of DeFi with SOY Finance. Our unique insurance fund provides a safe and affordableenvironment for trading, farming, and staking."/>
-      <meta name="twitter:image" content="https://soy-finance-landing.vercel.app/og.jpg"/>
+      <meta name="twitter:image" content="https://soy.finance/og.jpg"/>
       <meta name="twitter:image:alt" content="Soy.Finance | Where DeFi Meets Safety"/>
       <meta name="twitter:card" content="summary_large_image"/>
     </Head>
